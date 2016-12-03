18fad6fcc53017eb84a866a945d5a3227b18e2ca
Abdullah Khan <khanabdullahj@gmail.com>
2016-12-03 12:39:28 -0500

    Make comments neater

--- video_contour.py
+++ video_contour.py
@@ -11,18 +11,18 @@ def auto_canny(image, sigma=0.33):
 	return edged
 
 
-cap = cv2.VideoCapture(0)#Video cap object takes in the device # as a param
-#I'm assuming we both have only one camera on our laptops, so 0 is fine.
+cap = cv2.VideoCapture(0)  # Video cap object takes in the device # as a param
+# I'm assuming we both have only one camera on our laptops, so 0 is fine.
 
 if cap.isOpened():
 
 	while(True):
-		#Capture frame-by-frame
+		# Capture frame-by-frame
 		ret, frame = cap.read()
 		resized = imutils.resize(frame, width=300)
 		ratio = frame.shape[0] / float(resized.shape[0])
 
-		#Our operations on the frame come here
+		# Our operations on the frame come here
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
@@ -49,7 +49,7 @@ if cap.isOpened():
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
-	#When everything done, release the Capture
+	# When everything done, release the Capture
 	cap.release()
 	cv2.destroyAllWindows()
 else:
