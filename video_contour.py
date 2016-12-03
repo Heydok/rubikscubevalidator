6fa02d65ea526a6f064d35b585236f5578c4f1eb
Abdullah Khan <khanabdullahj@gmail.com>
2016-12-03 13:44:05 -0500

    Use auto_canny from pyimagesearch

--- video_contour.py
+++ video_contour.py
@@ -23,15 +23,16 @@ if cap.isOpened():
 	while(True):
 		# Capture frame-by-frame
 		ret, frame = cap.read()
-		resized = imutils.resize(frame, width=250)
+		resized = imutils.resize(frame, width=360)
 		ratio = frame.shape[0] / float(resized.shape[0])
 
 		# Our operations on the frame come here
 		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 		blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-		lab = cv2.cvtColor(resized, cv2.COLOR_BGR2LAB)
+		# lab = cv2.cvtColor(resized, cv2.COLOR_BGR2LAB)
 
-		im_binary = cv2.Canny(blurred, 30, 200)
+		# im_binary = cv2.Canny(blurred, 30, 200)
+		im_binary = auto_canny(blurred)
 
 		(thresh, im_binary) = cv2.threshold(im_binary, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
@@ -43,17 +44,17 @@ if cap.isOpened():
 				cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 				for c in cnts:
 					# Calculate the center of the contour.
-					M = cv2.moments(c)
-					cX = int((M["m10"] / M["m00"] + 1e-7) * ratio)
-					cY = int((M["m01"] / M["m00"] + 1e-7) * ratio)
+					# M = cv2.moments(c)
+					# cX = int((M["m10"] / M["m00"] + 1e-7) * ratio)
+					# cY = int((M["m01"] / M["m00"] + 1e-7) * ratio)
 					# Detect the shape.
 					shape = sd.detect(c)
 					if shape == 'Square':  # We only care about squares.
-						cv2.drawContours(resized, [c], -1, (0,255,0), 1)
-						color = cl.label(lab, c)
-						text = '{} {}'.format(color, shape)
-						cv2.putText(resized, text, (cX, cY),
-							cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+						cv2.drawContours(resized, [c], -1, (0,255,0), 2)
+						# color = cl.label(lab, c)
+						# text = '{} {}'.format(color, shape)
+						# cv2.putText(resized, text, (cX, cY),
+						# 	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 				while(screen_cap):
 					cv2.imshow("Contour", resized)
 					if cv2.waitKey(1) & 0xFF == ord('x'):
@@ -61,7 +62,7 @@ if cap.isOpened():
 						cv2.destroyWindow("Contour")
 
 		else:
-			cv2.imshow("Video", resized)
+			cv2.imshow("Live Feed", im_binary)
 					
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
@@ -69,5 +70,6 @@ if cap.isOpened():
 	# When everything done, release the Capture
 	cap.release()
 	cv2.destroyAllWindows()
+
 else:
 	print "ERROR! Videocapture didn't open for some godless reason"
