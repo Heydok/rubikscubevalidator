ced4059ca5abbf8ccf1f69845ed384d453d89072
jose <jseuribe@hotmail.com>
2016-12-03 11:40:13 -0500

    Combined several scripts

--- video_contour.py
+++ video_contour.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import imutils
+from shapedetector import ShapeDetector
 
 def auto_canny(image, sigma=0.33):
 	v = np.median(image)
@@ -17,6 +19,8 @@ if cap.isOpened():
 	while(True):
 		#Capture frame-by-frame
 		ret, frame = cap.read()
+		resized = imutils.resize(frame, width=300)
+		ratio = frame.shape[0] / float(resized.shape[0])
 
 		#Our operations on the frame come here
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -29,8 +33,26 @@ if cap.isOpened():
 		combined = cv2.addWeighted(wide, 0.7, tight, 0.3, 0)
 		combined = cv2.addWeighted(combined, 0.7, auto, 0.3, 0)
 		(thresh, im_binary) = cv2.threshold(combined, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-		cv2.imshow("Edges", im_binary)
 
+
+		cnts = cv2.findContours(im_binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		sd = ShapeDetector()
+
+		for c in cnts:
+			M = cv2.moments(c)
+			cX = int((M["m10"] / (M["m00"] + 1))* (ratio))
+			cY = int((M["m01"] / (M["m00"] + 1)) * (ratio))
+			shape = sd.detect(c)
+
+			c = c.astype("float")
+			c *= ratio
+			c = c.astype("int")
+			cv2.drawContours(im_binary, [c], -1, (0,255,0), 2)
+			cv2.putText(im_binary, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+			cv2.imshow("Image", im_binary)
+				
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
