bc93ba2b3c842ab433392db9d5d81463cb830f3f
jose <jseuribe@hotmail.com>
2016-12-06 00:22:47 -0500

    slightly modified gray scale to detect edges. still not great. Limited number of results for eliminating strange outliers

--- video_contour.py
+++ video_contour.py
@@ -24,41 +24,42 @@ if cap.isOpened():
 		# Capture frame-by-frame
 		ret, frame = cap.read()
 		resized = imutils.resize(frame, width=250)
+		pre_cnt_check_frame = resized
+
 		ratio = frame.shape[0] / float(resized.shape[0])
 
 		# Our operations on the frame come here
 		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 		blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-		lab = cv2.cvtColor(resized, cv2.COLOR_BGR2LAB)
+		#lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
 
-		im_binary = cv2.Canny(resized, 20, 100)
+		im_binary = cv2.Canny(blurred, 30, 200)
 
 		(thresh, im_binary) = cv2.threshold(im_binary, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-
-		if cv2.waitKey(1) & 0xFF == ord('c'):
-				screen_cap = True
-
-				cnts = cv2.findContours(im_binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-				cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-				for c in cnts:
-					# Detect the shape.
-					shape = sd.detect(c)
-					if shape == 'Square':  # We only care about squares.
-						x,y,w,h = cv2.boundingRect(c)
-						im_b_cpy = resized[y:y+h,x:x+w]
-						print("FOUND A SQUARE!!!!!!!!!!!!!!!!!!!")
-						cv2.drawContours(resized, [c], -1, (0,255,0), 1)
-						boundingRect = cv2.boundingRect(c)
-				while(screen_cap):
-					cv2.imshow("Contour", resized)
-					cv2.imshow("Square", im_b_cpy)
-					if cv2.waitKey(1) & 0xFF == ord('x'):
-						screen_cap = False
-						cv2.destroyWindow("Contour")
-
-		else:
-			cv2.imshow("Live Feed", im_binary)
+		cnts = cv2.findContours(im_binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+			#print(len(cnts))
+		#Sort the list from largest contours to smallest, and check only the first 10
+		#Ideally, there should only ever be 9, for each panel of a rubik face
+		cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+		for c in cnts:
+			# Detect the shape.
+			shape = sd.detect(c)
+			if shape == 'Square':  # We only care about squares.
+				x,y,w,h = cv2.boundingRect(c)
+				im_b_cpy = resized[y:y+h,x:x+w]
+				#print("FOUND A SQUARE!!!!!!!!!!!!!!!!!!!")
+				cv2.drawContours(resized, [c], -1, (0,255,0), 1)
+				boundingRect = cv2.boundingRect(c)
+				if cv2.waitKey(1) & 0xFF == ord('c'):
+					screen_cap = True
+					while(screen_cap):
+						cv2.imshow("Square", im_b_cpy)
+						if cv2.waitKey(1) & 0xFF == ord('x'):
+							screen_cap = False
+							cv2.destroyWindow("Contour")			
+		cv2.imshow("Live Feed", resized)
 
 					
 		if cv2.waitKey(1) & 0xFF == ord('q'):
